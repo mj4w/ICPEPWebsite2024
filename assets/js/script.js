b85b272ec750724c51027eb9c9d6fd72a8b7b0fc
bloodaste <kurtandrei50@gmail.com>
2024-05-07 09:58:35 +0800

    aboutus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,73 @@
-var userFunc = document.getElementById("user");
+const carousel = document.querySelector(".carousel"),
+    firstImg = carousel.querySelectorAll("img")[0],
+    arrowIcons = document.querySelectorAll(".wrapper i");
 
-userFunc.addEventListener("click", function() {
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
 
-document.getElementById("user-function").style.display = "block";
-document.getElementById("close-user").style.display = "block";
-this.style.display = "none";
-});
-
-let subMenu = document.getElementById("subMenu");
-
-function toggleMenu(){
-    subMenu.classList.toggle("open-menu");
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
 }
 
-
-// swiper js
-var swiper = new Swiper(".tools-slider", {
-    breakpoints: {
-        0: {
-            slidesPerView: 2,
-        },
-        768: {
-            slidesPerView: 3,
-        },
-        991: {
-            slidesPerView: 4,
-        },
-    },
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+    });
 });
 
-// view-more
-let loadMoreBtn = document.querySelector('#view-more');
-let currentItem = 3;
+const autoSlide = () => {
+    // if there is no image left to scroll then return from here
+    if (carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
 
-loadMoreBtn.onclick = () =>{
-    let boxes = [...document.querySelectorAll('.events .container .event-list .card')];
-    for (var i = currentItem; i < currentItem + 3; i++){
-        boxes[i].style.display = 'inline-block';
-    }
-    currentItem += 3;
+    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+    let firstImgWidth = firstImg.clientWidth + 14;
+    // getting difference value that needs to add or reduce from carousel left to take middle img center
+    let valDifference = firstImgWidth - positionDiff;
 
-    if(currentItem >= boxes.length){
-        loadMoreBtn.style.display = 'none';
+    if (carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
     }
+    // if user is scrolling to the left
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
 }
+
+const dragStart = (e) => {
+    // updatating global variables value on mouse down event
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    // scrolling images/carousel to left according to mouse pointer
+    if (!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+
+    if (!isDragging) return;
+    isDragging = false;
+    autoSlide();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+document.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
